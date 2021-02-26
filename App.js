2dd3bfbfbac4fcbe83d47017807801472fc2fa8f
Ryan Paige <oshmagoshzz@gmail.com>
2021-02-26 07:20:56 -0500

    formatting

--- App.js
+++ App.js
@@ -2,60 +2,56 @@ import {
   NavigationContainer,
   DarkTheme as NavigationDarkTheme,
   DefaultTheme as NavigationDefaultTheme,
-} from '@react-navigation/native';
-import { 
+} from "@react-navigation/native";
+import {
   DarkTheme as PaperDarkTheme,
   DefaultTheme as PaperDefaultTheme,
   Provider as PaperProvider,
-} from 'react-native-paper';
-import 'react-native-get-random-values';
-import 'react-native-gesture-handler';
-import { AppRegistry, StatusBar } from 'react-native';
-import React, { useState } from 'react';
-import { Provider } from 'react-redux';
-import {store, persistor} from './redux/store';
-import * as Font from 'expo-font';
-import { AppLoading } from 'expo';
-import Navigator from './routes/drawer';
-import { globalStyles } from './styles/global';
-import { PersistGate } from 'redux-persist/integration/react';
+} from "react-native-paper";
+import "react-native-get-random-values";
+import "react-native-gesture-handler";
+import { AppRegistry, StatusBar } from "react-native";
+import React, { useState } from "react";
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store";
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
+import Navigator from "./routes/drawer";
+import { globalStyles } from "./styles/global";
+import { PersistGate } from "redux-persist/integration/react";
 
-
-const getFonts = () => 
-   Font.loadAsync({
-    'roboto-regular': require( './assets/fonts/Roboto-Regular.ttf' ),
-    'roboto-black': require( './assets/fonts/Roboto-Black.ttf' ),
-    'roboto-bold': require( './assets/fonts/Roboto-Bold.ttf' ),
-    'roboto-medium': require( './assets/fonts/Roboto-Medium.ttf' ),
-    'roboto-italic': require( './assets/fonts/Roboto-Italic.ttf' ),
+const getFonts = () =>
+  Font.loadAsync({
+    "roboto-regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "roboto-black": require("./assets/fonts/Roboto-Black.ttf"),
+    "roboto-bold": require("./assets/fonts/Roboto-Bold.ttf"),
+    "roboto-medium": require("./assets/fonts/Roboto-Medium.ttf"),
+    "roboto-italic": require("./assets/fonts/Roboto-Italic.ttf"),
   });
-  //customize theme colors
-  // const CombinedDefaultTheme = merge( PaperDefaultTheme, NavigationDefaultTheme );
-  // const CombinedDarkTheme = merge( PaperDarkTheme, NavigationDarkTheme );
+//customize theme colors
+// const CombinedDefaultTheme = merge( PaperDefaultTheme, NavigationDefaultTheme );
+// const CombinedDarkTheme = merge( PaperDarkTheme, NavigationDarkTheme );
 export default function App() {
-  const [ fontsLoaded, setFontsLoaded ] = useState( false );
+  const [fontsLoaded, setFontsLoaded] = useState(false);
 
-  if( fontsLoaded ){
+  if (fontsLoaded) {
     return (
-      <Provider store={ store }>
-        <PersistGate loading={ null } persistor={ persistor }>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
           <PaperProvider>
             <NavigationContainer>
-              <Navigator style={ globalStyles.navbar }/>
+              <Navigator style={globalStyles.navbar} />
               <StatusBar animated={true} style="auto" />
             </NavigationContainer>
           </PaperProvider>
         </PersistGate>
-        </Provider>
+      </Provider>
     );
-  }else {
-      return (
-        <AppLoading
-          startAsync={ getFonts }
-          onFinish={() => setFontsLoaded( true )}
-        />
-      )
-  }  
+  } else {
+    return (
+      <AppLoading startAsync={getFonts} onFinish={() => setFontsLoaded(true)} />
+    );
+  }
 }
 
-AppRegistry.registerComponent( App, () => App );
+AppRegistry.registerComponent(App, () => App);
