bce5537963077d0a633a4f3d901d6ae9c087f53c
Ryan Paige <oshmagoshzz@gmail.com>
2021-04-04 20:57:45 -0400

    removed unneeded files

--- screens/todoScreen.js
+++ screens/todoScreen.js
@@ -1,17 +1,13 @@
 import React from "react";
 import { View } from "react-native";
 import { globalStyles } from "../styles/global";
-// import AddTodo from '../components/todos/addTodo';
 import { connect } from "react-redux";
-// import  TodoList  from '../components/todos/todoList';
 import ListMain from "../components/todos/listMain";
 
 export function TodoScreen() {
   return (
     <View style={globalStyles.todoScreenContainer}>
       <ListMain />
-      {/* <TodoList/> */}
-      {/* <AddTodo /> */}
     </View>
   );
 }
