647467b8f7f2f425bdd2ec32c7b1d29772d17391
Ryan Paige <oshmagoshzz@gmail.com>
2021-06-07 22:11:58 -0400

    initial convertion to typescript

--- screens/about.tsx
+++ screens/about.tsx
@@ -1,16 +1,26 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, StyleProp, TextStyle, ViewStyle, View } from "react-native";
 import { Card, Text } from "react-native-paper";
 import { coltsGray, coltsBlue } from "../styles/global";
 
-const aboutText =
+interface AboutProps {
+  aboutContainer: StyleProp<ViewStyle>;
+  text: StyleProp<TextStyle>;
+}
+
+interface Styles {
+  aboutContainer: ViewStyle;
+  text: TextStyle;
+}
+
+const aboutText: string =
   "This is an ongoing App based on charting any visions or dreams you may have. Notes tab for thoughts and todos.";
 
-const About = () => {
+const About: React.FC<AboutProps> = () => {
   return (
     <View style={{ backgroundColor: coltsGray, flex: 1 }}>
-      <Card style={styles.aboutContent}>
-        <Text multiline style={styles.text}>
+      <Card style={styles.aboutContainer}>
+        <Text style={styles.text}>
           {" "}
           {aboutText}{" "}
         </Text>
@@ -19,14 +29,14 @@ const About = () => {
   );
 };
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create<Styles>({
   text: {
     padding: 8,
     margin: 30,
     alignSelf: "center",
     color: coltsGray,
   },
-  aboutContent: {
+  aboutContainer: {
     backgroundColor: coltsBlue,
     flex: 0.4,
     flexDirection: "row",
