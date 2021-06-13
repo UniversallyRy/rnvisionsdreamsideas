d7b5604bce7337dc828b55398657544ff0eba217
Ryan Paige <oshmagoshzz@gmail.com>
2021-06-13 03:51:58 -0400

    consistency fix

--- redux/reducers/index.ts
+++ redux/reducers/index.ts
@@ -2,8 +2,8 @@ import { combineReducers } from "redux";
 import visions from "./visions";
 import pic from "./newpic";
 import journals from "./journals";
-import note from "./note";
+import notes from "./note";
 import todos from "./todos";
 
 // combinedReducer import on index.js for multiple for store connection on multiple reducers
-export default combineReducers({ visions, pic, journals, todos, note });
+export default combineReducers({ visions, pic, journals, todos, notes });
