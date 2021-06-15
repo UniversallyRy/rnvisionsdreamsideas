5a983ae57fa83934d0ca3a35b0a904385f15c13e
Ryan Paige <oshmagoshzz@gmail.com>
2021-06-15 15:25:25 -0400

    fixed note state

--- redux/reducers/index.ts
+++ redux/reducers/index.ts
@@ -2,8 +2,8 @@ import { combineReducers } from "redux";
 import visions from "./visions";
 import pic from "./newpic";
 import journals from "./journals";
-import notes from "./note";
+import note from "./note";
 import todos from "./todos";
 
 // combinedReducer import on index.js for multiple for store connection on multiple reducers
-export default combineReducers({ visions, pic, journals, todos, notes });
+export default combineReducers({ visions, pic, journals, todos, note });
