4a10c85533888432ef2dc7be444d123d371cc2d0
Ryan Paige <oshmagoshzz@gmail.com>
2022-01-12 16:59:26 -0500

    custom transition

--- routes/visionStack.tsx
+++ routes/visionStack.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent } from "react";
-import { createStackNavigator, StackNavigationProp, CardStyleInterpolators, HeaderStyleInterpolators, TransitionPresets} from "@react-navigation/stack";
+import { createStackNavigator, StackNavigationProp, StackNavigationOptions, TransitionSpecs } from "@react-navigation/stack";
 import Visions from "../screens/visionScreen";
 import VisionDetails from "../components/visions/visionDetails";
 import Header from "../shared/header";
@@ -17,24 +17,61 @@ type NavProp = {
 
 const Stack = createStackNavigator<VisionStackParamList>()
 
+const customTrans: StackNavigationOptions = {
+  gestureEnabled: true,
+  gestureDirection: "horizontal",
+  transitionSpec: {
+    open: TransitionSpecs.FadeInFromBottomAndroidSpec,
+    close: TransitionSpecs.FadeOutToBottomAndroidSpec,
+  },
+  cardStyleInterpolator: ({ current, next, layouts }) => {
+    return {
+      cardStyle: {
+        transform: [
+          {
+            translateX: current.progress.interpolate({
+              inputRange: [0, 1],
+              outputRange: [layouts.screen.width, 0],
+            }) 
+          },
+          {
+            rotate: current.progress.interpolate({
+              inputRange: [0, 1],
+              outputRange: ["40deg", "0deg"],
+            })
+          },
+          {
+            scale: next ? 
+              next.progress.interpolate({
+                inputRange: [0, 1],
+                outputRange: [1, 0.7],
+              }) : 1,
+          },
+        ]
+      }
+    }
+  }
+}
+
 const VisionStack: FunctionComponent<NavProp> = () => {
   return (
     <Stack.Navigator 
-      screenOptions={{
+      initialRouteName="VisionScreen"
+      screenOptions ={{
         headerMode:"screen",
         header: ({ route, previous, navigation }:any) => (
           <Header title="Visions " scene={route} previous={previous} navigation={navigation} />
-        ),
+          ),
+        ...customTrans,
       }}
-      initialRouteName="VisionScreen"
     >
       <Stack.Screen
         name="VisionScreen"
         component={Visions}
         options={{ 
-          ...TransitionPresets.DefaultTransition,
-          cardStyleInterpolator: CardStyleInterpolators.forRevealFromBottomAndroid, 
-          headerStyleInterpolator: HeaderStyleInterpolators.forUIKit,
+          // ...TransitionPresets.DefaultTransition,
+          // cardStyleInterpolator: CardStyleInterpolators.forRevealFromBottomAndroid, 
+          // headerStyleInterpolator: HeaderStyleInterpolators.forUIKit,
         }}
       />
       <Stack.Screen
@@ -42,9 +79,9 @@ const VisionStack: FunctionComponent<NavProp> = () => {
         component={VisionDetails}
         options={{ 
           headerTitle: 'Vision Details', 
-          ...TransitionPresets.ModalTransition,
-          cardStyleInterpolator: CardStyleInterpolators.forFadeFromBottomAndroid, 
-          headerStyleInterpolator: HeaderStyleInterpolators.forUIKit,
+          // ...TransitionPresets.ModalTransition,
+          // cardStyleInterpolator: CardStyleInterpolators.forFadeFromBottomAndroid, 
+          // headerStyleInterpolator: HeaderStyleInterpolators.forUIKit,
         }}
       />
     </Stack.Navigator>
