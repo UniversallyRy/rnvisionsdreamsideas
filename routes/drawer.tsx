825779b605e61e9937387197f12f576bfd9304f7
Ryan Paige <oshmagoshzz@gmail.com>
2022-01-26 12:40:39 -0500

    types fixed

--- routes/drawer.tsx
+++ routes/drawer.tsx
@@ -1,27 +1,25 @@
-import React from "react";
-import { StyleProp, ViewStyle } from "react-native";
-import { createBottomTabNavigator, BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
+import React, { FC } from "react";
+import { NavigationHelpers, ParamListBase } from "@react-navigation/native";
 import { BottomNavigation, BottomNavigationTab } from '@ui-kitten/components';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { BottomTabNavigationEventMap } from "@react-navigation/bottom-tabs/lib/typescript/src/types";
 import VisionStack from "./visionStack";
 import JournalStack from "./journalStack";
 import NoteStack from "./noteStack";
 // import { AboutStack } from "./aboutStack";
 
-type TabStackParamList = {
-  Visions: undefined;
-  Journals: undefined;
-  Notes: undefined;
-};
-
-type NavigationProp = BottomTabNavigationProp<TabStackParamList>;
-interface TabProps {
-  navigation?: NavigationProp;
-  style?: StyleProp<ViewStyle>;
+type NavigationProp = NavigationHelpers<ParamListBase, BottomTabNavigationEventMap>;
+export interface TabProps {
+  navigation: NavigationProp;
+  state: {
+    index: number
+    routeNames: string | string[]
+  }
 }
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
-const BottomTabBar = ({ navigation, state }) => (
+const BottomTabBar: FC<TabProps>= ({ navigation, state }) => (
   <BottomNavigation
     selectedIndex={state.index}
     onSelect={index => navigation.navigate(state.routeNames[index])}>
@@ -31,7 +29,7 @@ const BottomTabBar = ({ navigation, state }) => (
   </BottomNavigation>
 );  
 
-export const BottomTabs = () => (
+const BottomTabs = () => (
   <Navigator 
     screenOptions={{
       headerShown:false
@@ -43,3 +41,5 @@ export const BottomTabs = () => (
     <Screen name='Notes' component={NoteStack}/>
   </Navigator>
 );
+
+export default BottomTabs;
