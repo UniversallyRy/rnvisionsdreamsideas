d9e15d094c10cfd6e20767bfdcd43e5e4ab88c1a
Ryan Paige <oshmagoshzz@gmail.com>
2022-01-28 01:17:02 -0500

    reformats, card style changes

--- components/notes/todoLists.tsx
+++ components/notes/todoLists.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent, useState, useEffect } from "react";
-import { Modal, TextStyle, ViewStyle, StyleSheet, View } from "react-native";
+import { Modal, TextStyle, ViewStyle, StyleSheet } from "react-native";
 import { Card, Layout, Text } from "@ui-kitten/components";
 import { useDispatch } from "react-redux";
 import TodosModal from "./todosModal";
@@ -25,26 +25,26 @@ interface Styles {
 }
 
 const TodoLists: FunctionComponent<TodoListsProps>= ({ list }) => {
-  const dispatch = useDispatch()
   const [visible, setVisible] = useState(false);
   const [InitRemaining, setInitRemaining] = useState(0);
   const [InitCount, setInitCount] = useState(0);
   const [isMount, setIsMount] = useState(true);
+  const dispatch = useDispatch();
+
   const toggleListModal = () => {
     setVisible(!visible);
   };
+
   useEffect(()=>{
-          if(isMount){
-              let completedCount = (filtered(list.todos, (todo:any) => todo.completed));
-              let remainingCount = (filtered(list.todos, (todo:any) => !todo.completed));
-              setInitCount(Object.keys(completedCount).length);
-              setInitRemaining(Object.keys(remainingCount).length);
-              setIsMount(false);
-              return;
-          }
-      completedList(InitRemaining, InitCount);
-          
-          //Do anything here for 2nd render onwards
+    if(isMount){
+      let completedCount = (filtered(list.todos, (todo:any) => todo.completed));
+      let remainingCount = (filtered(list.todos, (todo:any) => !todo.completed));
+      setInitCount(Object.keys(completedCount).length);
+      setInitRemaining(Object.keys(remainingCount).length);
+      setIsMount(false);
+      return;
+    }
+    completedList(InitRemaining, InitCount);
   }, [TodosModal])
     
 const completedList = (remaining:number, completed:number) => {
@@ -52,9 +52,8 @@ const completedList = (remaining:number, completed:number) => {
   setInitCount(completed);
 }
   
-  const filtered = (obj:object, predicate:any) => {
-    let result = {},
-    key;
+  const filtered = (obj:object, predicate: any) => {
+    let result = {}, key: PropertyKey;
     
     for (key in obj) {
       if (obj.hasOwnProperty(key) && !predicate(obj[key])) {
@@ -75,19 +74,15 @@ const completedList = (remaining:number, completed:number) => {
         >
           <TodosModal completedList={ completedList } item={ list } closeModal={ () => toggleListModal() } />
         </Modal>
-        <View>
           <Text style={ styles.listTitle } numberOfLines={ 1 }>
             { list.name }
           </Text>
-            <View style={{ alignItems: "center" }} accessibilityLabel="Remaining Todo Count" >
+            <Card style={{ backgroundColor: list.color, borderColor: list.color }}>
               <Text style={ styles.count }>{ InitRemaining }</Text>
               <Text style={ styles.subtitle }>Remaining</Text>
-            </View>
-            <View style={{ alignItems: "center" }} accessibilityLabel="Completed Todo Count">
               <Text style={ styles.count }>{ InitCount }</Text>
               <Text style={ styles.subtitle }>Completed</Text>
-            </View>
-        </View>
+            </Card>
       </Card>
       <CloseButton
         style={ styles.deleteButton }
@@ -119,10 +114,12 @@ const styles = StyleSheet.create<Styles>({
   count: {
     fontSize: 40,
     fontWeight: "200",
+    alignSelf: "center"
   },
   subtitle: {
     fontSize: 10,
     fontWeight: "700",
+    alignSelf: "center"
   },
   deleteButton: {
     alignItems: "center",
