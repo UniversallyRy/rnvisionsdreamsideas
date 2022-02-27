efcd3ac2e5d1982a7c6c216e874b5f43b9b62de5
Ryan Paige <oshmagoshzz@gmail.com>
2022-02-27 06:10:16 -0500

    ts

--- App.test.tsx
+++ App.test.tsx
@@ -4,7 +4,7 @@ import App from './App';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
-it('renders without crashing', () => {
+it('renders without crashing', (): void => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
