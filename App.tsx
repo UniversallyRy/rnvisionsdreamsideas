fb30b3327fc83ed54f37a68a2000ca91809b8b3e
Ryan Paige <universallyry@gmail.com>
2023-03-11 20:19:00 -0500

    splash screen changes

--- App.tsx
+++ App.tsx
@@ -1,25 +1,64 @@
 import "react-native-get-random-values";
-import 'react-native-gesture-handler';
 import React, { useState } from "react";
 import { AppRegistry, StatusBar } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { Provider } from "react-redux";
-import * as Font from "expo-font";
-import AppLoading from 'expo-app-loading';
 import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import * as eva from '@eva-design/eva';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from "expo-font";
 import { ThemesContext } from "./ThemeContext";
 import BottomTabs from "./routes/Drawer";
 import { store, /*persistor */ } from "./redux/store";
 import { default as customTheme } from './styles/custom-theme.json';
 // import { PersistGate } from "redux-persist/integration/react";
+SplashScreen.preventAutoHideAsync();
 
 const App = (): JSX.Element => {
   const [fontsLoaded, setFontsLoaded] = useState(false);
+
   // const [persistLoaded, setPersistLoaded] = useState(true);
   const [theme, setTheme] = useState('light');
   const [evaTheme, setEvaTheme] = useState(eva.light);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Keep the splash screen visible while we fetch resources
+        await SplashScreen.preventAutoHideAsync();
+        // Pre-load fonts, make any API calls you need to do here
+        await Font.loadAsync({
+          "roboto-black": require("./assets/fonts/Roboto-Black.ttf"),
+          "roboto-bold": require("./assets/fonts/Roboto-Bold.ttf"),
+          "roboto-italic": require("./assets/fonts/Roboto-Italic.ttf"),
+          "roboto-medium": require("./assets/fonts/Roboto-Medium.ttf"),
+          "roboto-regular": require("./assets/fonts/Roboto-Regular.ttf"),
+        });
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+        setFontsLoaded(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
 
   const toggleTheme = (): void => {
     const nextTheme = theme === 'light' ? 'dark' : 'light';
@@ -27,44 +66,21 @@ const App = (): JSX.Element => {
     setTheme(nextTheme);
   };
 
-  const getFonts = (): Promise<void> => {
-    return Font.loadAsync({
-      "roboto-black": require("./assets/fonts/Roboto-Black.ttf"),
-      "roboto-bold": require("./assets/fonts/Roboto-Bold.ttf"),
-      "roboto-italic": require("./assets/fonts/Roboto-Italic.ttf"),
-      "roboto-medium": require("./assets/fonts/Roboto-Medium.ttf"),
-      "roboto-regular": require("./assets/fonts/Roboto-Regular.ttf"),
-    });
-  };
-
-  if (fontsLoaded) {
     return (
       <Provider store={store}>
         <ThemesContext.Provider value={{ theme, toggleTheme }}>
           <IconRegistry icons={EvaIconsPack} />
           <ApplicationProvider {...eva} theme={{ ...evaTheme, ...customTheme }}>
             <StatusBar animated={true} />
-            <NavigationContainer>
+            <NavigationContainer onReady={onLayoutRootView}>
               <BottomTabs />
             </NavigationContainer>
           </ApplicationProvider>
         </ThemesContext.Provider>
       </Provider>
     );
-  } else {
-    return (
-      <AppLoading
-        startAsync={getFonts}
-        onFinish={(): void => {
-          setFontsLoaded(true);
-          // setPersistLoaded(false);
-        }}
-        onError={console.warn}
-      />
-    );
-  }
 };
 
-export default App;
-
 AppRegistry.registerComponent("App", () => App);
+
+export default App;
